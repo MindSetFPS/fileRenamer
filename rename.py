47de5cd2618a892c29faf4d5826d6e42042da269
DanieEscobar <daanielescobar@gmail.com>
2023-01-04 16:34:50 -0600

    Update for more reusability.

--- rename.py
+++ rename.py
@@ -1,18 +1,21 @@
 import os 
 
-
-#/TV Shows/ShowName/Season 02/ShowName – s02e17 – Optional_Info.ext
-
-s = 's01e'
-
+# Loop trough the files in the current directory
 for f in os.listdir():
-    if f == "rename.py":
-        continue
+    # Get file name and file extension
     f_name, f_ext = os.path.splitext(f)
-    la, patrona, num = f_name.split()
-    num = num.zfill(3)
+    # Get directory name
+    path = os.path.basename(os.getcwd())
+    
+    if f_ext == '.mp4':
+        # Remove all the text after the first space 
+        file_name = f_name.split(" ", 1)[0]
 
-    new_name = '{} {} - {}{}{}'.format(la, patrona, s, num, f_ext )
+        #Remove all the text after the first _
+        file_name = file_name.split("_", 1)[0] 
+        print(file_name) 
 
-    os.rename(f, new_name)
-    # print(f)#
+        # Contatenate
+        new_name = path + 'E' + file_name + f_ext
+        print(new_name)
+        os.rename(f, new_name)
